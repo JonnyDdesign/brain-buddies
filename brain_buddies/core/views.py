714d3c3f3033b965ec216a9762169edc70e90f49
JonnyDdesign <jondesserres@gmail.com>
2024-08-27 15:10:20 -0700

    updated views.py to interact with serializers and handle API requests

--- brain_buddies/core/views.py
+++ brain_buddies/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import MathChallenge, CoinTransaction, Pet
+from .serializers import MathChallengeSerializer, CoinTransactionSerializer, PetSerializer
 
-# Create your views here.
+class MathChallengeViewSet(viewsets.ModelViewSet):
+    queryset = MathChallenge.objects.all()
+    serializer_class = MathChallengeSerializer
+
+class CoinTransactionViewSet(viewsets.ModelViewSet):
+    queryset = CoinTransaction.objects.all()
+    serializer_class = CoinTransactionSerializer
+
+class PetViewSet(viewsets.ModelViewSet):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
