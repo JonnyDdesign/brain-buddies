a2b80acf261959ba005e88692916a90fc021e895
JonnyDdesign <jondesserres@gmail.com>
2024-08-30 09:38:34 -0700

    Updated the views.py file to add a ScienceChallenge viewset

--- brain_buddies/core/views.py
+++ brain_buddies/core/views.py
@@ -1,10 +1,14 @@
 from rest_framework import viewsets
-from .models import MathChallenge, CoinTransaction, Pet
-from .serializers import MathChallengeSerializer, CoinTransactionSerializer, PetSerializer
+from .models import MathChallenge, ScienceChallenge, CoinTransaction, Pet
+from .serializers import MathChallengeSerializer, ScienceChallengeSerializer, CoinTransactionSerializer, PetSerializer
 
 class MathChallengeViewSet(viewsets.ModelViewSet):
     queryset = MathChallenge.objects.all()
     serializer_class = MathChallengeSerializer
+
+class ScienceChallengeViewSet(viewsets.ModelViewSet):
+    queryset = ScienceChallenge.objects.all()
+    serializer_class = ScienceChallengeSerializer
 
 class CoinTransactionViewSet(viewsets.ModelViewSet):
     queryset = CoinTransaction.objects.all()
